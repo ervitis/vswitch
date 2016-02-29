0f25bccf4fdf85bd54b91a1757518a59f72f4e97
ervitis <vitomarti@gmail.com>
2016-02-29 21:42:03 +0100

    feat: setup file not empty test

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -7,16 +7,18 @@ from .data_tests import PROJECT_PATH
 class TestSetupVSwitch(unittest.TestCase):
 
     def setUp(self):
-        pass
+        import os
+        self.path_setup_script = os.path.join(PROJECT_PATH, 'setup.py')
 
     def tearDown(self):
         pass
 
     def test_setup_exists(self):
-        import os
-        path_setup_script = os.path.join(PROJECT_PATH, 'setup.py')
+        self.assertTrue(self.path_setup_script)
 
-        self.assertTrue(os.path.exists(path_setup_script))
+    def test_setup_file_is_not_empty(self):
+        with open(self.path_setup_script) as setup_file:
+            self.assertGreater(len(setup_file.readlines()), 0)
 
 
 if __name__ == '__main__':
